fcae43eeb20cfd9a723cd1fd7f1d16c7dd2c83f5
work stanley.man.hk <work.stanley.man.hk@outlook.com>
2019-01-16 03:25:54 +0800

    MonthExpense Class Added

---
+++ src/MonthExpense.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using SQLitePCL;
+
+namespace MyHomeExpense
+{
+
+    public partial class MainPage : ContentPage
+    {
+        static DatabaseExpense database;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public class DatabaseExpense
+        {
+            public DatabaseExpense(string dbPath)
+            {
+                database = new SQLite.SQLiteAsyncConnection(dbPath);
+                database.CreateTableAsync<TodoItem>().Wait();
+            }
+        }
+    }
+
+
+
+    public class MonthExpense
+    {
+        public enum CategoryExpense : int { Unknown = 0, HouseHold = 1, OnlineShop = 2, Kids = 3, Education = 4, OutSchool = 5, Special = 6, Others = 7 };
+        public enum TypeExpense : int { Unknown = 0, HouseHold = 1, OnlineShop = 2, Kids = 3, Education = 4, OutSchool = 5 };
+
+        public DateTime Month { get; set; }
+        public double TotalAmount { get; set; }
+        public double HouseHold { get; set; }
+        public double OnlineShop { get; set; }
+        public double Kids { get; set; }
+        public double Education { get; set; }
+        public double OutSchool { get; set; }
+        public double Special { get; set; }
+        public double Others { get; set; }
+
+        public class Expense
+        {
+            public DateTime InputTime { get; set; }
+            public TypeExpense Type { get; set; }
+            public double Amount { get; set; }
+        }
+    }
+}
